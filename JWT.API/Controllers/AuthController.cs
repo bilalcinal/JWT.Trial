c86e4be1ef835e14479fbbed7aae037d448b4d4e
Bilal cinal <73084109+bilalcinal@users.noreply.github.com>
2023-08-11 13:19:38 +0300

    Jwt token trşal

---
+++ JWT.API/Controllers/AuthController.cs
@@ -0,0 +1,73 @@
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization.Infrastructure;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+
+namespace JWT.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AuthController : ControllerBase
+    {
+        string signinKey = "BuBenimSigningKey";
+
+        [HttpGet]
+        public string Get(string userName, string Password)
+        {
+
+            var Claims = new[]{
+                new Claim(ClaimTypes.Name, userName),
+                new Claim(JwtRegisteredClaimNames.Email, userName)
+            };
+            
+            var securitykey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(signinKey));
+            var credentials = new SigningCredentials(securitykey, SecurityAlgorithms.HmacSha256);
+
+            var jwtSecurityToken = new JwtSecurityToken(
+                issuer: "https://www.fatihbaytar.com",
+                audience:"BuBenimKullandığımTestDeğeri",
+                claims: Claims,
+                expires: DateTime.Now.AddDays(15),
+                notBefore: DateTime.Now,
+                signingCredentials: credentials
+            );
+ 
+           var token = new JwtSecurityTokenHandler().WriteToken(jwtSecurityToken);
+           return token;
+        }
+
+        
+        [HttpGet("ValidateToken")]
+        public bool ValidateToken(string token)
+        {
+            var securtiyKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(signinKey));
+            try
+            {
+                JwtSecurityTokenHandler handler = new();
+                handler.ValidateToken(token, new TokenValidationParameters{
+                  ValidateIssuerSigningKey = true,
+                  IssuerSigningKey = securtiyKey,
+                  ValidateLifetime = true,
+                  ValidateAudience = false,
+                  ValidateIssuer = false,
+                }, out SecurityToken validatedToken);
+                
+                var jwtToken =  (JwtSecurityToken)validatedToken;
+                var claims = jwtToken.Claims.ToList();
+                return true;
+            }
+            catch (System.Exception)
+            {
+                
+                return false;
+            }
+
+        }
+    }
+}
---
+++ JWT.API/Program.cs
@@ -0,0 +1,51 @@
+﻿using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+//auth
+var configuration = builder.Configuration;
+var jwtConfig = configuration.GetSection("JwtConfig");
+
+var issuer = jwtConfig["Issuer"];
+var audience = jwtConfig["Audience"];
+var signingKey = jwtConfig["SigningKey"];
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options =>
+{
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true,
+        ValidIssuer = issuer,
+        ValidAudience = audience,
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(signingKey))
+    };
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
